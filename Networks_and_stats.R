d85a05cb60c8af7431ac1542ca4cbfd0087af925
Monica Berggren <mjbergs@gmail.com>
2022-07-18 11:52:37 -0600

    added optional community trimming flag, more clustering options for network, changed network visualization

--- Networks_and_stats.R
+++ Networks_and_stats.R
@@ -10,44 +10,48 @@ getScriptPath <- function(){
   if(length(script.dir) > 1) stop("can't determine script dir: more than one '--file' argument detected")
   return(script.dir)
 }
-# Set the script path:
-working_dir<- paste0(getScriptPath(),"/") #added the "/" at the end so paste0 commands below work.
-#working_dir<-"/Users/esforsythe/Documents/Work/Bioinformatics/ERC_networks/Analysis/ERCnet_dev/"
-setwd(working_dir)
 
-#Because the above function returns "." as the working dir, using this command to set the full path (to avoid issues below)
-working_dir<-paste0(getwd(),"/")
+
+### Read in all user-defined variables here
 
 #Read in arguments
 args = commandArgs(trailingOnly=TRUE)
 
 #Get the job name (used to identify the proper output folder)
 jobname<-args[1]
-#jobname<-"BIGTEST"
-#jobname<-"TPC_test"
-#jobname<-"lab_meeting"
-out_dir<-paste0("OUT_", jobname, "/")
-
-#Read in ERC correlation results
-#Read the table
-ERC_results_df<-read.table(file = paste0(working_dir, out_dir, "ERC_results/ERC_results.tsv"), header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 
 #Get parameters for filtering data to be used in networks
 BL_type<-paste(args[2])
 filter_stat<-paste(args[3])
 filter_stat_cutoff<-as.numeric(paste(args[4]))
 
-###Development***
-##BLtype
-#BL_type<-"bxb"
-#BL_type<-"r2t"
-#
-##Stats
-#filter_stat<-"pval"
-#filter_stat_cutoff<-0.001
-#OR
-#filter_stat<-"R2"
-#filter_stat_cutoff<-0.8
+#Get the method to be used
+clust_method<-args[5]
+
+#User defined trim cutoff (i.e. what minimum community size is displayed)
+trim_cutoff <- as.numeric(paste(args[6]))
+
+#Get the ID of the focal species
+foc_sp<-paste(args[7])
+
+###
+
+
+
+# Set the script path:
+working_dir<- paste0(getScriptPath(),"/") #added the "/" at the end so paste0 commands below work.
+
+setwd(working_dir)
+
+#Because the above function returns "." as the working dir, using this command to set the full path (to avoid issues below)
+working_dir<-paste0(getwd(),"/")
+
+out_dir<-paste0("OUT_", jobname, "/")
+
+#Read in ERC correlation results
+#Read the table
+ERC_results_df<-read.table(file = paste0(working_dir, out_dir, "ERC_results/ERC_results.tsv"), header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+
 
 #Filter data to be included in ERC network
 if(BL_type == "bxb"){
@@ -75,46 +79,93 @@ print("Printing networks...")
 #Build network
 network_graph<-graph.data.frame(ERC_hits_df, directed = FALSE)
 
-#degree(network_graph)
-#Cluster communities
-#Get the method to be used
-clust_method<-args[5]
-#clust_method<-"fg"
-#clust_method<-"eb"
+
+
+#CLUSTER COMMUNITIES
 
 #Cluster
 if(clust_method == "fg"){
   comms<-cluster_fast_greedy(network_graph)
+  algo_name <- "fast and greedy"
 }else if(clust_method == "eb"){
   comms<-cluster_edge_betweenness(network_graph)
+  algo_name <- "edge betweenness"
+}else if(clust_method == "op"){
+  comms<-cluster_optimal(network_graph)
+  algo_name <- "optimal"
+}else if(clust_method == "wt"){
+  comms<-cluster_walktrap(network_graph)
+  algo_name <- "walktrap"
+}else{
+  comms<-cluster_fast_greedy(network_graph)
+  algo_name <- "fast and greedy"
 }
 
-#dendPlot(comms, mode = "hclust")
-#membership(comms)
-#length(comms)
+
+#TRIM OR NO TRIM (if statement)
+
+if (trim_cutoff > 0){
+ 
+  comms_keep_ids <- as.numeric(names(sizes(comms)[sizes(comms) >= trim_cutoff]))
+  comms_keep_v_idxs <- which(comms$membership %in% comms_keep_ids)
+  
+  network_graph_final <- induced_subgraph(network_graph, V(network_graph)[comms_keep_v_idxs])
+  
+  # subset community objects
+  comms_final <- comms
+  comms_final$names <- comms$names[comms_keep_v_idxs]
+  comms_final$membership <- comms$membership[comms_keep_v_idxs]
+  comms_final$vcount <- length(comms_final$names)
+  comms_final$modularity <- modularity(network_graph_final, comms_final$membership, E(network_graph_final)$weight)
+  
+  #Network layout and colors
+  LO <- layout_nicely(network_graph)
+  LO_final <- LO[comms_keep_v_idxs, ]
+  comms_plot_col <- rainbow(length(communities(comms)), alpha = 0.3)[comms_keep_ids]
+  comms_plot_border <- rainbow(length(communities(comms)), alpha = 1)[comms_keep_ids]
+  
+  
+}else{
+    network_graph_final <- network_graph
+    comms_final <- comms
+    comms_plot_col <- rainbow(length(comms), alpha = 0.3)
+    comms_plot_border <- rainbow(length(comms), alpha = 1)
+    #Network layout
+    LO_final <- layout_nicely(network_graph_final)
+}
+
+
 
 #save pdf
 pdf(file = paste0(working_dir, out_dir, "Network_analyses/ERC_network_",BL_type, "_", filter_stat, "_", filter_stat_cutoff, "_", clust_method,".pdf"), width=8, height = 8)
 
-#plot the graph
-plot.igraph(network_graph, 
+#Plot the graph with all communities
+plot(comms_final, network_graph_final,
             vertex.label=NA,
             vertex.size=5,
-            edge.color="black",
-            vertex.color=membership(comms), 
-            #layout=layout.fruchterman.reingold,
-            layout=layout_nicely,
             vertex.label.color="black",
-            main=paste0("BL method: ",BL_type,
-                        "\nFilter stat: ", filter_stat,
-                        "\nCutoff: ", filter_stat_cutoff,
-                        "\n", length(comms), " communities clustered by: ", clust_method))
+            edge.color="black",
+            V(network_graph)$color<-"black",
+            mark.col = comms_plot_col,
+            mark.border = comms_plot_border,
+            layout=LO_final,
+            main=paste0(length(comms_final), " communities clustered using ", algo_name, " algorithm"))
+
+mysubtitle<-paste0("BL method: ",BL_type, "  |  ", "Filter stat: ", filter_stat, "  |  ", "Cutoff: ", filter_stat_cutoff)
+mtext(side = 3, line = 0, at = 1, adj = 1, mysubtitle)
+
+legend("topleft",
+       legend = as.factor(1:length(comms)),
+        fill = rainbow(length(comms), alpha = 0.3))
+
 
 dev.off()
 
+
+
 ## Get the genes in each community
 #Create df
-comms_df<-data.frame(HOG=comms$names, Community=comms$membership)
+comms_df<-data.frame(HOG=comms_final$names, Community=comms_final$membership)
 
 #Reorder
 comms_df<-comms_df[order(comms_df$Community),]
@@ -127,11 +178,6 @@ All_HOGs_df$HOG<-sapply(strsplit(as.character(All_HOGs_df$HOG), "\\."), `[`, 2)
 
 #Join the dataframes
 comms_w_IDs<-merge(x = comms_df, y = All_HOGs_df, by="HOG", all.x=TRUE)
-
-#Get the ID of the focal species
-foc_sp<-paste(args[6])
-#foc_sp<-"Atha"
-#foc_sp<-"A_thaliana_prot"
 
 #Get a dataframe with the focal sp IDs
 foc_sp_df<-cbind(comms_w_IDs[,c(1,2,3)],
@@ -159,3 +205,5 @@ for(n in min(foc_sp_df$Community):max(foc_sp_df$Community)){
 } 
   
   
+
+
